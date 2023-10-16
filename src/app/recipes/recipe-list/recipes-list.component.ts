d46e9ed592a6a2bb8480ab9711773a09e1f1beda
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2023-10-15 21:17:29 -0300

    .

--- src/app/recipes/recipe-list/recipes-list.component.ts
+++ src/app/recipes/recipe-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -10,9 +11,11 @@ import { RecipeService } from '../recipe.service';
 export class RecipesListComponent {
   recipes: Recipe[] = [];
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) {}
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
   }
-
+  onNewRecipe() {
+    this.router.navigate(['new']), {relativelo: this.route};
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,8 +9,8 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
+      'Pizza de Carne de Sol',
+      'A melhor pizza de carna de sol do mundo',
       'https://receitasdepizza.com.br/wp-content/uploads/2023/02/Mito-Pizza-Carne-de-sol-com-Nata.webp',
       [
         new Ingredient('Ovos', 6),
@@ -20,6 +20,20 @@ export class RecipeService {
         new Ingredient('Extrato de tomate', 1),
         new Ingredient('Orégano', 1),
         new Ingredient('Carne de Sol', 2),
+        new Ingredient('Manteiga', 1),
+        new Ingredient('Óleo', 1)
+      ]
+    ),
+    new Recipe(
+      'Bomba de Teresina',
+      'Melhor bomba do mundo',
+      'https://s2-g1.glbimg.com/Y00P_edQsqKLybzLWxjs7AQWjgA=/0x0:1920x1280/984x0/smart/filters:strip_icc()/s.glbimg.com/jo/g1/f/original/2015/06/26/img_0349.jpg',
+      [
+        new Ingredient('Ovos', 6),
+        new Ingredient('Trigo', 1),
+        new Ingredient('Queijo', 4),
+        new Ingredient('Fermento', 1),
+        new Ingredient('Presunto', 2),
         new Ingredient('Manteiga', 1),
         new Ingredient('Óleo', 1)
       ]
@@ -36,4 +50,8 @@ export class RecipeService {
   addIngredientToShoppingList(ingrediets: Ingredient[] ){
     this.slService.addIngredients(ingrediets);
   }
+  getRecipe(index: number){
+    return this.recipes[index];
+  }
 }
+
